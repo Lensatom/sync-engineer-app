d1ffeba682501de978740a7720c16e33ee8ab3f8
lensatom <ayomidetommiwa@gmail.com>
2026-03-13 20:28:15 +0100

    feat: enhance Tab and Avatar components with improved styling and functionality; refactor Tasks screen for better readability and maintainability

--- components/ui/avatar.tsx
+++ components/ui/avatar.tsx
@@ -1,19 +1,35 @@
-import React from 'react';
-import { Image } from 'react-native';
+import { textToColor } from "@/helpers/utils";
+import React from "react";
+import { Image } from "react-native";
+import { Text, View } from "tamagui";
 
 type AvatarProps = {
-  uri: string;
+  uri?: string;
+  text?: string;
   size?: number;
-}
+};
 
-export function Avatar({
-  uri,
-  size = 40,
-}: AvatarProps) {
+export function Avatar({ uri, text = "Avatar", size = 40 }: AvatarProps) {
+  if (!uri) {
+    return (
+      <View
+        w={size}
+        h={size}
+        bg={textToColor(text, 90)}
+        ai="center"
+        jc="center"
+        br={999}
+      >
+        <Text fos={size / 2} fow="700" color={textToColor(text, 30)}>
+          {text[0]}
+        </Text>
+      </View>
+    );
+  }
   return (
     <Image
       source={{ uri }}
       style={{ width: size, height: size, borderRadius: 99 }}
     />
-  )
+  );
 }
